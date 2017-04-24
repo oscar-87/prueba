63141a0727a8c45f5d058fedd90fb5c910cf0437
oscar-87 <oscar_garciamartin@hotmail.com>
2017-04-24 14:06:26 +0200

    Modificado el script.sh

--- script.sh
+++ script.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-lineaInclude=""
-ll="Hola "
-pp="que tal"
-for i in `git branch -r| grep -v HEAD|cut -d "/" -f 2`; do
-   echo $i
-   lineaInclude+="$i "
-done
-
+lineaInclude=$(git config --local --get pull.default)
 echo $lineaInclude
